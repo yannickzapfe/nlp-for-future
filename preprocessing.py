eb3c037b2e66a1b4deb06724dfd4fd8f97a1790f
Yannick Zapfe <yannickzapfe@posteo.de>
2022-10-19 14:09:45 +0000

    Remove stop words when tokenizing.

--- preprocessing.py
+++ preprocessing.py
@@ -7,17 +7,19 @@ from imblearn.under_sampling import RandomUnderSampler
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import np_utils
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 
 # Load first n book reviews including the review text and score in a dataframe.
 def load_books_rating_data(first_n):
-    if first_n and ('data/reviews' + str(first_n) + '.csv') in os.listdir():
+    if first_n and ('reviews' + str(first_n) + '.csv') in os.listdir('data'):
         return pd.read_csv('data/reviews' + str(first_n) + '.csv')
 
     books_data_raw = pd.read_csv('data/Books_rating.csv')
 
     books_data = books_data_raw[['review/score', 'review/text']].copy()
-    books_data.rename(columns={'review/score': 'score', 'review/text': 'text'}, inplace=True)
+    books_data.rename(columns={'review/score': 'score', 'review/text': 'review'}, inplace=True)
 
     # Remove rows with null values.
     books_data = books_data.dropna()
@@ -25,16 +27,15 @@ def load_books_rating_data(first_n):
     return books_data.head(first_n)
 
 
-def nb_tokenize(books_data):
-    return books_data.text.apply(lambda sent: nltk.word_tokenize(sent))
+def tokenize(sents, remove_stop_words=True):
+    sents_tokenized = [word_tokenize(sent.lower()) for sent in sents]
 
+    if not remove_stop_words:
+        return sents_tokenized
 
-def nn_tokenize(books_data):
-    review_texts = books_data.text
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(review_texts)
-    review_texts_tokenized = tokenizer.texts_to_sequences(review_texts)
-    return review_texts_tokenized, tokenizer
+    # Remove english stop words.
+    stop_words = list(stopwords.words("english")) + [",", ".", "(", ")", "'s", "&"]
+    return [[word for word in sent if word not in stop_words] for sent in sents_tokenized]
 
 
 # Pad tokenized review texts to have the same length.
@@ -52,9 +53,12 @@ def flatten(sents):
     return [word for sent in sents for word in sent]
 
 
-def remove_stop_words(words):
-    stop_words = list(nltk.corpus.stopwords.words('english')) + [",", ".", "(", ")", "'s", "&"]
-    return [word.lower() for word in words if word.lower() not in stop_words]
+def get_stop_words():
+    return list(nltk.corpus.stopwords.words('english')) + [",", ".", "(", ")", "'s", "&"]
+
+
+def remove_stop_words_from_words(words):
+    return [word.lower() for word in words if word.lower() not in get_stop_words()]
 
 
 def get_review_features(X_balanced, words, most_common_n=2000):
@@ -94,8 +98,23 @@ def create_embeddings_matrix(word_index):
 
 def nn_preprocess(first_n):
     books_data = load_books_rating_data(first_n)
-    X_tokenized, tokenizer = nn_tokenize(books_data)
-    X_padded = nn_pad(X_tokenized, None)
-    X_balanced, y_balanced = under_sample(X_padded, books_data.score)
-    embeddings_matrix = create_embeddings_matrix(tokenizer.word_index)
+
+    X_balanced, y_balanced = under_sample(np.array(books_data.text).reshape(-1, 1), books_data.score)
+    X_balanced = [sent[0] for sent in X_balanced]
+
+    X_tokenized = tokenize(X_balanced)
+
+    vocab = set([word for sent in X_tokenized for word in sent])
+    word_index = {}
+    index = 1
+    for word in vocab:
+        word_index[word] = index
+        index += 1
+
+    X_indexed = [[word_index[word] for word in sent] for sent in X_tokenized]
+
+    X_padded = nn_pad(X_indexed, None)
+
+    embeddings_matrix = create_embeddings_matrix(word_index)
+
     return X_balanced, y_balanced, embeddings_matrix
